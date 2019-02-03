6f36400b488d2191ce2116c46d5455a4d527d5dd
Vitalii Maksimov <g.vitaly.maximov@gmail.com>
2019-02-03 13:33:51 +0300

    ViewModel project with MvvmLight is added

---
+++ BoardGameTimer/BoardGameTimer.ViewModel/MainViewModel.cs
@@ -0,0 +1,9 @@
+using GalaSoft.MvvmLight;
+
+namespace BoardGameTimer.ViewModel.ViewModel
+{
+    public class MainViewModel : ViewModelBase
+    {
+        public string Message { get; } = "Hello world!";
+    }
+}
---
+++ BoardGameTimer/BoardGameTimer.ViewModel/ViewModelLocator.cs
@@ -0,0 +1,28 @@
+using CommonServiceLocator;
+using GalaSoft.MvvmLight.Ioc;
+
+namespace BoardGameTimer.ViewModel.ViewModel
+{
+    /// <summary>
+    /// This class contains static references to all the view models in the
+    /// application and provides an entry point for the bindings.
+    /// </summary>
+    public class ViewModelLocator
+    {
+        /// <summary>
+        /// Initializes a new instance of the ViewModelLocator class.
+        /// </summary>
+        public ViewModelLocator()
+        {
+            ServiceLocator.SetLocatorProvider(() => SimpleIoc.Default);
+
+            SimpleIoc.Default.Register<MainViewModel>();
+        }
+
+        public MainViewModel MainViewModel => ServiceLocator.Current.GetInstance<MainViewModel>();
+
+        public static void Cleanup()
+        {
+        }
+    }
+}
